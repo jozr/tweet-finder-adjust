849b5991768be9141039c890b01ddfda900401e6
Josephine Wright <jozright@gmail.com>
2017-11-03 12:40:47 +0100

    Add some error handling

--- server.rb
+++ server.rb
@@ -1,21 +1,34 @@
 require "bundler"
-
 Bundler.require
-Dotenv.load
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
+Dotenv.load
 
 get "/" do
   erb :index
 end
 
-get "/search"  do
-  @tweets = client.search("#{params[:query]}", result_type: "recent").take(10)
+get "/search" do
+  query = params[:query]
 
-  erb :index
+  if query_present?(query)
+  	@tweets = client.search("#{query}", result_type: "recent").take(10)
+  	@info_message = "No matching tweets were found." if @tweets.empty?
+
+  	erb :index
+  else
+  	@info_message = "Enter some text to search tweets."
+
+  	erb :index
+  end
+end
+
+def client
+  @client ||= Twitter::REST::Client.new do |config|
+    config.consumer_key    = ENV["CONSUMER_KEY"]
+    config.consumer_secret = ENV["CONSUMER_SECRET"]
+  end
+end
+
+def query_present?(query)
+  query && !query.strip.empty?
 end
